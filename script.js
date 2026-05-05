008a4ea14db3dfe7f63fef93495fbf35110b7a3e
EliteFeeLInG <kevinpomian@icloud.com>
2026-05-05 03:33:27 +0200

    add: js code add: css sheet add: code html

--- script.js
+++ script.js
@@ -3,32 +3,41 @@ let health = 100
 let money = 50
 let RepKit = 0
 
-function getStatus() {
-    console.log("Name of the Ship: " + NameShip);
-    console.log("Health: " + health);
-    console.log("Money: " + money);
-    console.log("Ship RepKid: " + RepKit);
+function renderStatus() {
+    const status = document.getElementById("status");
+    status.innerHTML = `
+        <table>
+            <tr><td>Name</td><td>${NameShip}</td></tr>
+            <tr><td>Leben</td><td>${health}</td></tr>
+            <tr><td>Gold</td><td>${money}</td></tr>
+            <tr><td>Tränke</td><td>${RepKit}</td></tr>
+        </table>
+    `
 }
 
-function getDamage(amount) {
+function getDamage() {
+    const amount = document.getElementById("inputfield").value;
     health = health - amount;
-    console.log("You got damage: " + amount);
-
+    if (health <= 0) {
+        document.getElementById("status").classList.add("death");
+        health = 0;
+    }
+    getStatus();
 }
 
-function buyRepKit(amount) {
+function buyRepKit() {
     if (money >= amount) {
         RepKit = RepKit + amount;
         money = money - amount;
-    console.log("You got a RepKit : " + amount);
-        
+        console.log("You got a RepKit : " + amount);
+
     }
     else {
         console.log("You don´t have enough Money!!");
     }
 }
 
-function getRepKit(amount) {
+function getRepKit() {
     if (RepKit >= amount) {
         RepKit = RepKit - amount
         health = health + amount
@@ -37,10 +46,3 @@ function getRepKit(amount) {
         console.log("You don´t have enough RepKits!! Please go and buy a few");
     }
 }
-
-
-getStatus();
-getDamage(30);
-getStatus();
-getRepKit(1);
-buyRepKit(1);
